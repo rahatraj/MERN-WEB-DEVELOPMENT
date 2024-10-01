727a487d9a73abecf22003424f5c2dcf887d6e58
Rahat Reza <68533251+rahatraj@users.noreply.github.com>
2024-10-01 20:28:21 +0530

    Delete backend directory

--- backend/app/middlewares/secretkeyAuthenticate.js
+++
@@ -1,7 +0,0 @@
-export  default function secretKeyAuthencate(req,res,next){
-    const secretKey = req.headers['x-secret-key']
-    if(!secretKey || secretKey !== process.env.SECRET_KEY){
-        return res.status(403).json({error : 'Access denied. Invalid secret key'})
-    }
-    next();
-}
